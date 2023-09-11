9c15e5c7f07b28f139fd5f71a76c71910cd521e0
ZkillerZJT <87343938+ZkillerZJT@users.noreply.github.com>
2023-09-11 11:50:01 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hola mundo!")
+}
+func placeholder(){
+	fmt.Println("hijos del pico")
+}
+func placeholder1(){
+	fmt.Println("hijos del pico")
+}
